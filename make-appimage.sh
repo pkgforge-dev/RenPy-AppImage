f1a91b0392a0b7659870f16b4fe0c8a64357e37e
Link4Electronics <link4electronics@gmail.com>
2026-03-31 13:25:48 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-#VERSION=$(pacman -Q renpy | awk '{print $2; exit}') # example command to get version of application here
-export ARCH #VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -15,7 +14,7 @@ export DEPLOY_SYS_PYTHON=1
 
 # Deploy dependencies
 chmod +x ./AppDir/bin/renpy.py
-quick-sharun ./AppDir/bin/* #./AppDir/bin/renpy.py
+quick-sharun ./AppDir/bin/*
 
 # Additional changes can be done in between here
 
